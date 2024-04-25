8904a1f783d1aeb97ddeed2dfbfbe52a8fd8c317
ezekuiel100 <ezekuiel100@gmail.com>
2024-04-25 14:29:51 -0300

    Nav bar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,16 @@
-export default function Admin() {
+import { Nav, NavLink } from "@/components/Nav";
+import { ReactNode } from "react";
+
+export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <nav></nav>;
+      <Nav>
+        <NavLink href={"/admin"}>Dashboard</NavLink>
+        <NavLink href={"/admin/products"}>Products</NavLink>
+        <NavLink href={"/admin/users"}>Customers</NavLink>
+        <NavLink href={"/admin/orders"}>Sales</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import AdminLayout from "./admin/layout";
 
 export default function Home() {
   return (
     <>
-      <h1>Hi </h1>
+      <h1>Hi</h1>
     </>
   );
 }
